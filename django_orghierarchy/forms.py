9f68e3167f449e8847d2e7d462024f48863a9df0
Mingfeng Yu <mingfeng.yu@anders.fi>
2017-11-28 15:22:15 +0200

    Add replaced_by to Organization
    Admin can only select a non-replaced organizaion as replaced by target

--- django_orghierarchy/forms.py
+++ django_orghierarchy/forms.py
@@ -11,13 +11,17 @@ class OrganizationForm(forms.ModelForm):
             'data_source', 'origin_id', 'classification',
             'name', 'founding_date', 'dissolution_date',
             'parent', 'responsible_organization', 'admin_users',
-            'regular_users',
+            'regular_users', 'replaced_by',
         )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # An organization cannot be replaced by an organization that have already been replaced
+        self.fields['replaced_by'].queryset = Organization.objects.filter(replaced_by__isnull=True)
+
         # prevent self recursive references
         if self.instance.id:
             self.fields['parent'].queryset = Organization.objects.exclude(id=self.instance.id)
             self.fields['responsible_organization'].queryset = Organization.objects.exclude(id=self.instance.id)
+            self.fields['replaced_by'].queryset = self.fields['replaced_by'].queryset.exclude(id=self.instance.id)
--- tests/test_forms.py
+++ tests/test_forms.py
@@ -18,3 +18,15 @@ class TestOrganizationForm(TestCase):
         form = OrganizationForm(instance=self.organization)
         self.assertQuerysetEqual(form.fields['parent'].queryset, [])
         self.assertQuerysetEqual(form.fields['responsible_organization'].queryset, [])
+
+    def test_replaced_by_field_queryset_exclude_already_replaced(self):
+        OrganizationFactory(replaced_by=self.organization)
+        organization_2 = OrganizationFactory()
+
+        form = OrganizationForm()
+        queryset = form.fields['replaced_by'].queryset
+        self.assertQuerysetEqual(queryset, [repr(self.organization), repr(organization_2)], ordered=False)
+
+        form = OrganizationForm(instance=organization_2)
+        queryset = form.fields['replaced_by'].queryset
+        self.assertQuerysetEqual(queryset, [repr(self.organization)])

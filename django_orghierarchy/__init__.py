c21347f608797d272419ba5c3d5c6c4f81f8f638
Mingfeng Yu <mingfeng.yu@anders.fi>
2017-11-29 11:17:58 +0200

    Bump version to 0.1.1

--- django_orghierarchy/__init__.py
+++ django_orghierarchy/__init__.py
@@ -2,6 +2,6 @@ from .utils import get_data_source_model  # noqa
 
 default_app_config = 'django_orghierarchy.apps.DjangoOrghierarchyConfig'
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = ['get_data_source_model']
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 from setuptools import find_packages, setup
 
+from django_orghierarchy import __version__
+
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
@@ -9,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-orghierarchy',
-    version='0.1.0',
+    version=__version__,
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',

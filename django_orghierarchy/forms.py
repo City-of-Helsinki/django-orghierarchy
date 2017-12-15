2c57a9565daa1608f830c30c31c75a97dea05706
Mingfeng Yu <mingfeng.yu@anders.fi>
2017-12-15 16:57:12 +0200

    Fine grained permission check for affiliated organizations
    Introduce custom permissions relates to affiliated organizations to Organization model. These permissions are mainly checked when editing organizations in djagn admin. Users assigned with affiliated organization permissions can only add/change/remove affiliated organizations in his own admin_organizations.

--- django_orghierarchy/forms.py
+++ django_orghierarchy/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import widgets
 from django.utils.translation import ugettext as _
 
 from .models import Organization
@@ -18,20 +19,53 @@ class OrganizationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # An organization cannot be replaced by an organization that have already been replaced
-        self.fields['replaced_by'].queryset = Organization.objects.filter(replaced_by__isnull=True)
+        # the fields can be dynamically exclude, for example set them to readonly in admin
+        if 'replaced_by' in self.fields:
+            # An organization cannot be replaced by an organization that have already been replaced
+            qs = Organization.objects.filter(replaced_by__isnull=True)
+            if self.instance.id:
+                # prevent self reference
+                qs = qs.exclude(id=self.instance.id)
+            self.fields['replaced_by'].queryset = qs
 
-        # prevent self recursive references
-        if self.instance.id:
-            self.fields['parent'].queryset = Organization.objects.exclude(id=self.instance.id)
-            self.fields['replaced_by'].queryset = self.fields['replaced_by'].queryset.exclude(id=self.instance.id)
+        if 'parent' in self.fields and self.instance.id:
+            # prevent recursive reference
+            desc_ids = self.instance.get_descendants(include_self=True).values_list('id', flat=True)
+            self.fields['parent'].queryset = Organization.objects.exclude(id__in=desc_ids)
 
     def clean(self):
         cleaned_data = super().clean()
-        internal_type = cleaned_data['internal_type']
-        parent = cleaned_data['parent']
+        internal_type = cleaned_data.get('internal_type')
+        parent = cleaned_data.get('parent')
 
         if internal_type == Organization.AFFILIATED and parent is None:
             raise forms.ValidationError(_('Affiliated organization must have a parent organization'))
 
         return cleaned_data
+
+
+class SubOrganizationForm(forms.ModelForm):
+    default_internal_type = Organization.NORMAL
+
+    class Meta:
+        model = Organization
+        fields = (
+            'internal_type', 'data_source', 'origin_id',
+            'classification', 'name', 'founding_date',
+        )
+        widgets = {
+            'internal_type': widgets.HiddenInput,
+        }
+
+    def __init__(self, *args, **kwargs):
+        if 'initial' not in kwargs:
+            kwargs['initial'] = {}
+        kwargs['initial']['internal_type'] = self.default_internal_type
+        super().__init__(*args, **kwargs)
+
+    def clean_internal_type(self):
+        return self.initial['internal_type']  # do not allow changing internal_type
+
+
+class AffiliatedOrganizationForm(SubOrganizationForm):
+    default_internal_type = Organization.AFFILIATED
--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,11 @@
 from django.contrib.auth import get_user_model
 
 
-def make_admin(username='testadmin'):
+def make_admin(username='testadmin', is_superuser=True):
     user_model = get_user_model()
-    return user_model.objects.create(username=username, is_staff=True, is_superuser=True)
+    return user_model.objects.create(username=username, is_staff=True, is_superuser=is_superuser)
+
+
+def clear_user_perm_cache(user):
+    delattr(user, '_user_perm_cache')
+    delattr(user, '_perm_cache')

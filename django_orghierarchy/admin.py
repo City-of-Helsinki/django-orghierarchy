daa8927190a63c91ccd60fdedb894b8b0ffc2cce
Matti Eiden <matti.eiden@anders.com>
2023-02-09 10:16:51 +0200

    Remove deprecated use of ugettext_lazy

--- django_orghierarchy/admin.py
+++ django_orghierarchy/admin.py
@@ -1,7 +1,7 @@
 import swapper
 from django.contrib import admin
 from django.utils.html import format_html
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from mptt.admin import DraggableMPTTAdmin
 from functools import reduce
 
